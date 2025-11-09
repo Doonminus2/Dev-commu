cd1b47a336260365f92b39bfa47e242cbc4195af
Doonminus2 <piezapiezakok@gmail.com>
2025-11-09 14:37:29 +0700

    create expoloreBtn.tsx ✨

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import ExpoloreBtn from "@/components/ExpoloreBtn"
+
 const page = () => {
   return (
     <section>
       <h1 className="text-center ">
        Hub for Dev  <br /> Don't miss it !</h1>
       <p className="text-center mt-5">Hackathons , Meets , and conferneces , All in one place </p>
+      <ExpoloreBtn/>
     </section>
   )
 }
---
+++ components/ExpoloreBtn.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+const ExpoloreBtn = () => {
+  return (
+    <button onClick={() => console.log("click")}>ExpoloreBtn</button>
+  )
+}
+export default ExpoloreBtn
